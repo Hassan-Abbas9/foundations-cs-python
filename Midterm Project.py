fb4d87dfa37a6b78b07c4a8a7d161dbd70818818
Hassan <148596550+Hassan-Abbas9@users.noreply.github.com>
2023-11-09 22:46:55 +0300

    allowed opening multiple tabs and added a LIST for tabs

--- Midterm Project.py
+++ Midterm Project.py
@@ -1,8 +1,16 @@
-def open_new_tab():
-    title = input("please input the title of the new Tab : ")
-    url = input ("Please input the URL of the new Tab : ")
+tabs = []
 
-    new_tab = {title : url}
-    print("your open tabs", new_tab)
+def open_new_tab():
+    number = int(input("please enter the number of tabs : "))
+    for i in range(number):
+        title = input(f'Please input the TITLE of Tab {i + 1} : ')
+        url = input(f'Please input the URL of Tab {i + 1} : ')
+        new_tab = {title: url}
+        tabs.append(new_tab)
+        print(f"You opened {new_tab} tab successfully!")
+
+    print("your opened tabs are : ", tabs)
 
 open_new_tab()
+
+
