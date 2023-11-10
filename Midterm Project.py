46b97dde0bfdb7f099e9cc92b567699de8ebe953
Hassan <148596550+Hassan-Abbas9@users.noreply.github.com>
2023-11-10 17:15:14 +0300

    Imported BeautifulSoup libraries & completed the switch_tab function

--- Midterm Project.py
+++ Midterm Project.py
@@ -1,3 +1,6 @@
+import requests       # pip install requests used this in terminal  https://pypi.org/project/beautifulsoup4/
+from bs4 import BeautifulSoup      # pip install beautifulsoup4 used this in terminal
+
 tabs = []
 
 
@@ -19,10 +22,11 @@ open_new_tab()
 def close_tab():
     if not tabs:  # same as if tabs == [], simplified to fix "Expression can be simplified" weak warning.
         print("\nThere are no tabs open for you to close.")
-    else:
-        print("\nThe opened tabs are of the following indexes : ")
+        return
 
-    for i in range(len(tabs)):
+    print("\nThe opened tabs are of the following indexes : ")
+
+    for i in range(len(tabs)):     # for loop for displaying our tabs, so that we know which tab index to close.
         print(f"\n{i + 1}. {tabs[i]}")
 
     close_index = input("\nEnter the index of the tab you want to close / press ENTER to close the last tab : ")
@@ -46,8 +50,30 @@ def switch_tab():
 
     if not tabs:
         print("There are no tabs opened.")
+        return
 
     print("\nThe opened tabs are of the following indexes: ")
 
-    for i in range(len(tabs)):
+    for i in range(len(tabs)):    # for loop for displaying our tabs, so that we know which tab index to open.
         print(f"\n{i+1}. {tabs[i]}")
+
+    switch_index = input("Enter the index of the tab you want to display the HTML content for : ")
+    url = ""
+    if switch_index == "":
+        for key, value in tabs[len(tabs) - 1].items():
+            url = value
+    else:
+        switch_index = int(switch_index)
+        for key, value in tabs[len(tabs) - 1].items():
+            url = value
+        if 1 <= switch_index <= len(tabs):
+            print(f"\nYou opened the last tab {switch_tab}")
+            response = requests.get(url)   # Send an HTTP GET request to the URL of the selected tab - https://www.w3schools.com/PYTHON/ref_requests_get.asp
+            html_content = response.text       # Get the HTML content of the response content above
+            soup = BeautifulSoup(html_content, 'html.parser')   # https://www.crummy.com/software/BeautifulSoup/bs4/doc/#quick-start
+            print(soup.prettify())    # https://pypi.org/project/beautifulsoup4/ it is the BeautifulSoup object method that prettifies the content.
+        else:
+            print("Invalid Index. No tabs displayed.")
+
+
+switch_tab()
