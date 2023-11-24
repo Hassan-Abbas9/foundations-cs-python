c27dab83e8174f53380bf031382b99a12beaf6d5
Hassan <148596550+Hassan-Abbas9@users.noreply.github.com>
2023-11-24 17:02:22 +0300

    Added appending Node

--- Assignment#4.py
+++ Assignment#4.py
@@ -11,4 +11,13 @@ class LinkedList:
         self.tail = new_value
         self.length = 1
 
-
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        
