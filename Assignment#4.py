85a0a1e5883ea7aa0f42fd42ff4af83309c3897d
Hassan <148596550+Hassan-Abbas9@users.noreply.github.com>
2023-11-24 16:57:33 +0300

    Added class LinkedList

--- Assignment#4.py
+++ Assignment#4.py
@@ -2,4 +2,13 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_value = Node(value)
+        self.head = new_value
+        self.tail = new_value
+        self.length = 1
+
+
