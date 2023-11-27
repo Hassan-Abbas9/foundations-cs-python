12d13505dd366c0002c64bbe32e73d0cf4b0cff3
Hassan <148596550+Hassan-Abbas9@users.noreply.github.com>
2023-11-27 04:28:03 +0300

    Implemented the class for Graphs

--- Assignment#4.py
+++ Assignment#4.py
@@ -46,29 +46,126 @@ class LinkedList:
         self.length -= 1
         return temp
 
-def main():
-    value = int(input("Enter the initial value for the linked list: "))
-    linked_list = LinkedList(value)
+
+class Graph:
+    def __init__(self):
+        self.adj_list ={}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+
+
+def linked_list_menu():
 
     while True:
-        print("\na. Add Node")
-        print("b. Display Nodes")
-        print("c. Delete Node")
-        print("d. Return to main menu")
+        value = int(input("Enter the initial value for the linked list: "))
 
-        choice = input("Enter your choice: ").lower()
+        linked_list = LinkedList(value)
 
-        if choice == "a":
-            value = int(input("Enter the value to add: "))
-            linked_list.append(value)
-        elif choice == "b":
-            linked_list.print_nodes()
-        elif choice == "c":
-            value = int(input("Enter the value to INDEX of the value to delete: "))
-            linked_list.remove(value)
-        elif choice == "d":
-            break
+        while True:
+            print("\na. Add Node")
+            print("b. Display Nodes")
+            print("c. Delete Node")
+            print("d. Return to main menu")
+
+            sub_choice = input("Enter your choice: ").lower()
+
+            if sub_choice == "a":
+                value = int(input("Enter the value to add: "))
+                linked_list.append(value)
+            elif sub_choice == "b":
+                linked_list.print_nodes()
+            elif sub_choice == "c":
+                value = int(input("Enter the value to INDEX of the value to delete: "))
+                linked_list.remove(value)
+            elif sub_choice == "d":
+                break
+            else:
+                print("Invalid choice. Please enter a valid option.")
+
+
+def graph_menu():
+    graph = Graph()
+
+    while True:
+        sub_choice = input("\na. Add vertex\nb. Add edge\nc. Remove vertex\nd. Remove edge\ne. Display vertices with a degree of X or more.\nf. Return to main menu\nEnter your choice: ").lower()
+
+        if sub_choice == "a":
+            vertex = input("Enter the vertex to add: ")
+            if graph.add_vertex(vertex):
+                print(f"Vertex {vertex} added.")
+            else:
+                print(f"Vertex {vertex} already exists.")
+
+        elif sub_choice == "b":
+            v1 = input("Enter the first vertex: ")
+            v2 = input("Enter the second vertex: ")
+            if graph.add_edge(v1, v2):
+                print(f"Edge between {v1} and {v2} added.")
+            else:
+                print("Vertices do not exist.")
+
+        elif sub_choice == "c":
+            vertex = input("Enter the vertex to remove: ")
+            if graph.remove_vertex(vertex):
+                print(f"Vertex {vertex} removed.")
+            else:
+                print(f"Vertex {vertex} does not exist.")
+
+        elif sub_choice == "d":
+            v1 = input("Enter the first vertex: ")
+            v2 = input("Enter the second vertex: ")
+            if graph.remove_edge(v1, v2):
+                print(f"Edge between {v1} and {v2} removed.")
+            else:
+                print("Edge does not exist.")
+
+
         else:
             print("Invalid choice. Please enter a valid option.")
+
+
+def main():
+    print("1. Singly LinkedList \n2. Check Palindrome \n3. Priority Queue \n4. Evaluate an Infix Expression \n5. Graph \n6. Exit")
+
+    choice = int(input("\nEnter the choice you want : "))
+
+    if choice == 1:
+        linked_list_menu()
+
+    elif choice == 5:
+        graph_menu()
+
+    else:
+        print("Invalid choice. Please enter a valid option.")
+
 
 main()
